daa42166d39a58c3d2731c406e2f8d5dadb652ed
David Phillipus <david.n.phillipus@gmail.com>
2026-09-23 23:20:49 +0200

    Add Learning DSA arrays.js

---
+++ Learning DSA/arrays.js
@@ -0,0 +1,10 @@
+//Accessing time is O(1)
+//Inserting at position n in the array O(n) because you kind of shifts to ther right the other elements
+//Delete an item in positon n it's also O(n) shifting acurs too
+//Deleting an Item at the beginning or at the end O(1)
+//Initializing an array O(n)
+//Pop (means deletign at the end of an array) O(1)
+///Appending  (means adding at the end of the array) O(1)
+//Index Access O(1)
+//When you want to resize a fixed array just double the size of the array and then you can add(you are actually gonna create another array then copy then other one and then delete the original)
+6tgv
